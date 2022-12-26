ef2fe2857e6052cec176692b97c0feb67245d341
mirul786 <mirulkhan786@gmail.com>
2022-12-26 20:12:35 +0530

    temporarycomment

---
+++ problem1.js
@@ -0,0 +1,24 @@
+let num1 = 52;
+
+let num2 = 103;
+
+let num3 = 100235;
+
+
+if(num1>num2){
+  
+console.log(num1, "is greater")
+
+}
+
+else if(num2>num3){
+  
+console .log(num2, "is greater")
+
+}
+
+else{
+  
+console.log(num3, "is greatest")
+
+}
